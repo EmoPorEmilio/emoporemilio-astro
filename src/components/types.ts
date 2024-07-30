4f19992b1bcfbedd5f5e0217cd84038b57b2187e
EmoPorEmilio <franceschini.emilio@gmail.com>
2024-07-29 22:03:19 -0300

    message index, card changes

---
+++ src/components/types.ts
@@ -0,0 +1,10 @@
+
+export enum ButtonType {
+  Primary = 'primary',
+  Secondary = 'secondary',
+  Danger = 'danger',
+  Warning = 'warning',
+  Success = 'success',
+  Link = 'link',
+  Text = 'text',
+}
